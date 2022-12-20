80e9b6b178dca716f372f6e6f6a85ef15e299cc8
Mike Loseto <losetom1998@gmail.com>
2022-12-20 18:43:21 -0500

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,37 @@
 class StudentsController < ApplicationController
+    
+    # GET /students
+    def index
+        student = Student.all
+        render json: student
+    end
+
+    # GET /students/:id
+    def show
+        render json: @student, serializer: StudentSerializer
+    end
+
+    # POST /students
+    def create
+        student = Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    # PATCH /students/:id
+    def update
+        @student.update!(student_params)
+        render json: @student, status: :accetped
+    end
+
+    # DELETE /students/:id
+    def destroy
+        @student.destroy
+        head :no_content
+    end
+
+    private 
+
+    def set_student
+        @student = Student.find(parmas[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :instructors
   resources :students
-
-  get '/instrcutors', to instructors
-
 end
